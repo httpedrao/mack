9012cd67e1f2456b4d8052b965059e997d0bf9c3
Pedro Queiroz <eai@pedrao.dev>
2023-05-19 23:23:04 -0300

    Update main.js

--- FW/main.js
+++ FW/main.js
@@ -17,3 +17,31 @@ document.write(
 		<p>Localização - polo: ${campus}</p>
 	</footer>`
 );
+
+function sayHello() {
+  const weekDays = [
+    "domingo",
+    "segunda-feira",
+    "terça-feira",
+    "quarta-feira",
+    "quinta-feira",
+    "sexta-feira",
+    "sábado",
+  ];
+
+  function getGreeting(hour) {
+    if (hour >= 4 && hour < 12) return "Bom dia!";
+    if (hour >= 12 && hour < 18) return "Boa tarde!";
+    if (hour >= 18 || hour < 4) return "Boa noite!";
+  }
+
+  window.addEventListener("load", () => {
+    const hour = new Date().getHours();
+    const day = weekDays[new Date().getDay()];
+    const message = `${getGreeting(hour)} Hoje é ${day}.`;
+
+    alert(
+      `Me chamo ${fullName} \n\nSão Paulo - Análise e desenvolvimento de Sistemas / Universidade Presbiteriana Mackenzie\n\n${message}`
+    );
+  });
+}
